026b305e1c3849f94559a69784318b31d9e500f0
Sander van der Burg <svanderburg@gmail.com>
2017-07-24 23:54:09 +0200

    Use settings from the config + use multiple stylesheets

--- src/cms/picturepicker.php
+++ src/cms/picturepicker.php
@@ -1,13 +1,15 @@
 <?php
-set_include_path("./lib/sblayout:./lib/sbdata:./lib/sbgallery:./lib/sbeditor");
+set_include_path("./lib/sblayout:./lib/sbdata:./lib/sbgallery:./lib/sbeditor:./includes");
+
+require_once("config.inc.php");
 
 require_once("includes/model/MyGallery.class.php");
 require_once("gallery/view/html/picturepickerpage.inc.php");
 
-$dbh = new PDO("mysql:host=localhost;dbname=cms", "root", "admin", array(
+$dbh = new PDO($config["dbDsn"], $config["dbUsername"], $config["dbPassword"], array(
 	PDO::ATTR_PERSISTENT => true
 ));
 
 $myGallery = new MyGallery($dbh);
-displayPicturePickerPage($myGallery, "Gallery", "lib/layout/styles/default.css");
+displayPicturePickerPage($myGallery, "Gallery", array("lib/layout/styles/default.css", "lib/layout/styles/gallery.css"));
 ?>
