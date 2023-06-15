1a1faedf7a7c6f5a0df90197633a7141e67db845
Sander van der Burg <svanderburg@gmail.com>
2023-06-15 21:18:54 +0200

    Enable pagination in the gallery

--- src/portal/includes/Model/Page/MyGalleryPage.php
+++ src/portal/includes/Model/Page/MyGalleryPage.php
@@ -11,7 +11,30 @@ class MyGalleryPage extends GalleryPage
 {
 	public function __construct(AuthorizationManager $authorizationManager, PDO $dbh)
 	{
-		parent::__construct($dbh, new GalleryPageSettings("gallery"), new MyGalleryPermissionChecker($authorizationManager), new GalleryContents(null, "contents", "HTML", array("gallery.css")));
+		parent::__construct($dbh, new GalleryPageSettings(
+			"gallery", /* Base URL */
+			"gallery", /* Base directory */
+			160, 160, /* Thumbnail size */
+			1280, 1280, /* Picture size */
+			0666, /* File permissions */
+			0777, /* Directory permissions */
+			true, /* Display anchors */
+			"image/gallery", /* Icons path */
+			"album", /* Album anchor prefix */
+			"picture", /* Picture anchor prefix */
+			null, /* Gallery page labels */
+			null, /* Album page labels */
+			null, /* Picture page labels */
+			null, /* Gallery labels */
+			null, /* Album labels */
+			null, /* Picture labels */
+			null, /* Album editor settings */
+			null, /* Picture editor settings */
+			null, /* Album editor labels file */
+			null, /* Picture editor labels file */
+			20, /* Gallery page size */
+			20 /* Album page size */
+		), new MyGalleryPermissionChecker($authorizationManager), new GalleryContents(null, "contents", "HTML", array("gallery.css")));
 	}
 }
 ?>
